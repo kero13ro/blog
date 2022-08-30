c5a7c0acab6865a99c3e37e2e854d3afbb1e6c42
kero13ro <32509189+kero13ro@users.noreply.github.com>
2022-08-31 00:28:12 +0800

    update with puppeteer-extra

--- node_puppeteer/main.js
+++ node_puppeteer/main.js
@@ -1,5 +1,31 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer-extra")
+
 const TARGET_URL = 'https://wwwq.moex.gov.tw/exam/wFrmExamQandASearch.aspx';
+const UserPreferencesPlugin = require("puppeteer-extra-plugin-user-preferences");
+
+const downloadImageDirectoryPath = process.cwd();
+
+puppeteer.use(
+  UserPreferencesPlugin({
+    userPrefs: {
+      download: {
+        prompt_for_download: false,
+        open_pdf_in_system_reader: true,
+        default_directory: downloadImageDirectoryPath,
+        // automatic_downloads: 1,
+      },
+      plugins: {
+        always_open_pdf_externally: true,
+      },
+      // disable allow-multiple-downloads popup
+      profile: {
+        default_content_setting_values: {
+          automatic_downloads: 1,
+        },
+      },
+    },
+  })
+);
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -10,25 +36,27 @@ const TARGET_URL = 'https://wwwq.moex.gov.tw/exam/wFrmExamQandASearch.aspx';
   await page.goto(TARGET_URL);
 
   await page.click("#ctl00_holderContent_ibtnFull")
-  await page.waitForTimeout(500);
+  await page.waitForTimeout(300);
   await page.type("#ctl00_holderContent_txtCategoryName", "圖書資訊管理")
-  await page.waitForTimeout(500);
+  await page.waitForTimeout(300);
   await page.click("#ctl00_holderContent_btnQuery")
-  await page.waitForTimeout(500);
+  await page.waitForTimeout(300);
   await page._client().send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: './downloads' });
 
 
-  await page.$$eval("a.exam-question-ans[title$='試題(Pdf檔)']", (arr) => {
-    console.log(arr.map(el => el.href))
-
-    arr.forEach(ele => {
-      const link = document.createElement("a");
-      link.setAttribute("download", "data.pdf");
-      document.body.appendChild(link);
-      link.setAttribute("href", ele.href);
-      link.click();
-    })
+  const urls = await page.$$eval("a.exam-question-ans[title$='試題(Pdf檔)']", (arr) => {
+    return arr.map(el => el.href)
   });
+
+  for (let i = 0; i < urls.length; i++) {
+    const url = urls[i];
+    try {
+      await page.goto(`${url}`);
+      await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    } catch (error) {
+      // net::ERR_ABORTED
+    }
+  }
   await page.waitForTimeout(5000);
 
   // 爬取 考試名稱 選單
@@ -37,7 +65,7 @@ const TARGET_URL = 'https://wwwq.moex.gov.tw/exam/wFrmExamQandASearch.aspx';
   // });
   // console.log(photos)
 
-  // await browser.close();
+  await browser.close();
 })();
 
 
